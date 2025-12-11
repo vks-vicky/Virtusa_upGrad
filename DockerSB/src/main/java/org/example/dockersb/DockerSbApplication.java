3e1610e54a383db30a1625744e3e76d655eb91c2
Vivek KS <104208881+vks-vicky@users.noreply.github.com>
2025-12-11 12:11:57 +0530

    feat: SpringBoot Project- DockerSB

---
+++ DockerSB/src/main/java/org/example/dockersb/DockerSbApplication.java
@@ -0,0 +1,15 @@
+package org.example.dockersb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+
+@SpringBootApplication
+public class DockerSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerSbApplication.class, args);
+    }
+
+}
+
